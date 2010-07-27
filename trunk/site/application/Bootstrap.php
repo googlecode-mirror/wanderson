7c354ca4f4de5a74d9fb76f524c9077cfd517e88
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-07-27 17:50:56 +0000

    Exibição de Layout simples conforme padrões Dojo Toolkit e Dijit.

--- trunk/site/application/Bootstrap.php
+++ trunk/site/application/Bootstrap.php
@@ -20,6 +20,9 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     {
         $layout = $this->bootstrap('Layout')->getResource('Layout');
         $view   = $layout->getView();
+        Zend_Dojo::enableView($view);
+        $view->dojo()->enable()->setDjConfigOption('locale', 'pt-br')
+            ->setCdnVersion('1.5');
         $view->headTitle('Wanderson');
         $view->doctype(Zend_View_Helper_Doctype::XHTML1_STRICT);
         $view->headMeta()
