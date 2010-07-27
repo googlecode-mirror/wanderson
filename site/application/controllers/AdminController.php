ee5e9117cf8a73802c7c90916937b52114f56000
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-07-27 18:17:57 +0000

    Controladora administrativa do sistema, representante de usuários e postagens, bem como atualização do próprio sistema.

---
+++ site/application/controllers/AdminController.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * 
+ * Controladora Administrativa
+ * Administração do Sistema Sobre Usuários e Elementos Restritos
+ * @author     Wanderson Henrique Camargo Rosa
+ * @package    Application
+ * @subpackage Controller
+ * @see        http://code.google.com/p/wanderson/
+ *
+ */
+class AdminController extends Zend_Controller_Action
+{
+
+    /**
+     * 
+     * Inicialização da Controladora
+     * @return void
+     */
+    public function init()
+    {
+        
+    }
+
+    /**
+     * 
+     * Ação Principal da Controladora
+     * @return void
+     */
+    public function indexAction()
+    {
+        
+    }
+
+}
