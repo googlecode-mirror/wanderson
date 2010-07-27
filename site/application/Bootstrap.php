37f2f09f4aebc4abe9e825c31ba8da9a05f9cfae
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-07-27 16:50:27 +0000

    Inicialização do Layout do sistema. Configuração do inicializador do sistema para identificação de título da página e tipo de codificação de texto para interpretação do cliente.

--- site/application/Bootstrap.php
+++ site/application/Bootstrap.php
@@ -1,8 +1,28 @@
 <?php
 
+/**
+ * 
+ * Inicializador do Aplicativo
+ * Configura a Visualização do Sistema para Formatação e Estilo
+ * @author     Wanderson Henrique Camargo Rosa
+ * @package    Application
+ * @subpackage Bootstrap
+ * @see        http://code.google.com/p/wanderson/
+ */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-
-
+    /**
+     * 
+     * Inicialização dos Auxiliares da Camada de Visualização
+     * @return void
+     */
+    protected function _initViewHelpers()
+    {
+        $layout = $this->bootstrap('Layout')->getResource('Layout');
+        $view   = $layout->getView();
+        $view->headTitle('Wanderson');
+        $view->doctype(Zend_View_Helper_Doctype::XHTML1_STRICT);
+        $view->headMeta()
+            ->appendHttpEquiv('Content-Type','text/html; charset=UTF-8');
+    }
 }
-
