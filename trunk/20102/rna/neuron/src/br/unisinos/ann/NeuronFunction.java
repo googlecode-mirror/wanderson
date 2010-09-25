54f3d3f08399d2248a5bbfb278a27f12bcbc8082
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-09-25 17:55:48 +0000

    Inclusão de funções de transferência e documentação intracódigo

--- trunk/20102/rna/neuron/src/br/unisinos/ann/NeuronFunction.java
+++ trunk/20102/rna/neuron/src/br/unisinos/ann/NeuronFunction.java
@@ -2,17 +2,38 @@ package br.unisinos.ann;
 
 import java.lang.reflect.Method;
 
+/**
+ * Funções de Transferência
+ * 
+ * Encapsulamento das funções que são acessadas por padrão reflexível disponível
+ * na linguagem.
+ * 
+ * @author Wanderson Henrique Camargo Rosa
+ */
 public enum NeuronFunction
 {
-    THRESHOLD ("threshold");
+    THRESHOLD ("threshold"), SIGMOID ("sigmoid"), HYPERBOLIC ("hyperbolic");
 
+    /**
+     * Nome da Função
+     */
     private final String name;
 
+    /**
+     * Construtor do Elemento Enumerável
+     * @param name Nome da Função Desejada
+     */
     private NeuronFunction(String name)
     {
         this.name = name;
     }
 
+    /**
+     * Acesso Encapsulado para a Função de Transferência
+     * @param input Valor de Entrada
+     * @return Saída Esperada para a Função Escolhida
+     * @throws AnnException Função Inválida
+     */
     public double transfer(double input) throws AnnException
     {
         double result;
@@ -30,8 +51,36 @@ public enum NeuronFunction
         return result;
     }
 
-    public double threshold(Double input)
+    /**
+     * Função Rampa
+     * @param input Valor de Entrada
+     * @return Saída Esperada
+     */
+    @SuppressWarnings("unused")
+    private double threshold(Double input)
     {
         return input >= 0 ? 1 : 0;
+    }
+
+    /**
+     * Função Sigmóide
+     * @param input Valor de Entrada
+     * @return Saída Esperada
+     */
+    @SuppressWarnings("unused")
+    private double sigmoid(Double input)
+    {
+        return 1/(1 + Math.pow(Math.E,(-1 * input)));
+    }
+
+    /**
+     * Função Hiperbólica
+     * @param input Valor de Entrada
+     * @return Saída Esperada
+     */
+    @SuppressWarnings("unused")
+    private double hyperbolic(Double input)
+    {
+        return Math.tanh(input);
     }
 }
