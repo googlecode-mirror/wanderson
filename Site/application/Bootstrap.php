f72f5edb60e801534587fa765258ea173a67aaab
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-10-25 19:25:00 +0000

    Classe de inicialização do sistema e centralizadora de código que é executado em tempo de construção do aplicativo

---
+++ Site/application/Bootstrap.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Inicializador do Aplicativo
+ * 
+ * Nesta classe devem ser desenvolvidos os métodos que são inicializados em
+ * tempo de construção do aplicativo. Configurações que são necessárias em todo
+ * o ambiente de execução devem ser inseridas neste local, utilizando a notação
+ * conforme documentação do Zend Framework
+ * 
+ * @author Wanderson Henrique Camargo Rosa
+ */
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    
+}
