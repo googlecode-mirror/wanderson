bbe90683b6f479222ad8a4de35c44b643d98920b
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-10-25 19:47:04 +0000

    Inclusão de método para inclusão de tratamento de rotas do aplicativo, que buscam melhorar o acesso externo às seções e facilitar visualização

--- Site/application/Bootstrap.php
+++ Site/application/Bootstrap.php
@@ -9,8 +9,36 @@
  * conforme documentação do Zend Framework
  * 
  * @author Wanderson Henrique Camargo Rosa
+ * @package Application
+ * @subpackage Bootstrap
+ * @see http://code.google.com/p/wanderson/
  */
 class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
 {
-    
+    /**
+     * Rotas do Sistema
+     * 
+     * Insere novas rotas do sistema, tentando modificar os parâmetros
+     * solicitados para novos caminhos mais fáceis de identificar pelo usuário
+     * 
+     * @return void
+     */
+    public function _initRoutes()
+    {
+        /*
+         * Requisição de Início da Controladora
+         * Objeto de Tratamento de Rotas do Sistema
+         */
+        $this->bootstrap('FrontController');
+        $front = $this->getResource('FrontController');
+        $router = $front->getRouter();
+
+        /*
+         * Rota para Descrição do Aplicativo
+         */
+        $route = new Zend_Controller_Router_Route_Static('sobre',
+            array('controller' => 'index', 'action' => 'sobre')
+        );
+        $router->addRoute('sobre', $route);
+    }
 }
