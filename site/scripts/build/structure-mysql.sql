e3348b109189871646cf805b250bf900f2e69b98
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-10-25 19:04:55 +0000

    Remoção do site desenvolvido anteriormente. Ele será construído do zero, com melhorias e utilizando a ferramenta Zend Tool.

--- site/scripts/build/structure-mysql.sql
+++
@@ -1,9 +0,0 @@
--- migration people 8 lines
-CREATE TABLE people
-(
-    id       BIGINT      PRIMARY KEY AUTO_INCREMENT NOT NULL,
-    username VARCHAR(20) NOT NULL UNIQUE,
-    password CHAR(32)    NOT NULL,
-    deleted  BOOLEAN     DEFAULT FALSE,
-    active   BOOLEAN     DEFAULT TRUE
-);
