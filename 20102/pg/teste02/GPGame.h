e5ff5404e84958d55f88c1d1c11ec9c0b391fb77
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-11-04 17:43:06 +0000

    Divisão dos arquivos de source e ignorar compilados

--- 20102/pg/teste02/GPGame.h
+++ 20102/pg/teste02/GPGame.h
@@ -1,5 +1,3 @@
-#include <GL/glut.h>
-
 class GPGame
 {
     private:
@@ -14,49 +12,3 @@ class GPGame
     static GPGame* getInstance(void);
     void run(void);
 };
-
-GPGame* GPGame::instance = NULL;
-
-int GPGame::WIDTH  = 640;
-int GPGame::HEIGHT = 480;
-const char* GPGame::NAME = "Graphis Processing";
-
-GPGame::GPGame()
-{
-    glutInitDisplayMode(GLUT_SINGLE);
-    glutInitWindowSize(WIDTH,HEIGHT);
-    int x = (glutGet(GLUT_SCREEN_WIDTH) - WIDTH)/2;
-    int y = (glutGet(GLUT_SCREEN_HEIGHT) - HEIGHT)/2;
-    glutInitWindowPosition(x,y);
-    glutCreateWindow(NAME);
-    glutDisplayFunc(display);
-    glutReshapeFunc(reshape);
-}
-
-void GPGame::display(void)
-{
-    glClear(GL_COLOR_BUFFER_BIT);
-    glFlush();
-}
-
-void GPGame::reshape(int width, int height)
-{
-    glViewport(0, 0, width, height);
-    glMatrixMode(GL_PROJECTION);
-    glLoadIdentity();
-    gluPerspective(120, width / (height * 1.0), 1, 100);
-    glMatrixMode(GL_MODELVIEW);
-}
-
-GPGame* GPGame::getInstance()
-{
-    if (instance == NULL) {
-        instance = new GPGame();
-    }
-    return instance;
-}
-
-void GPGame::run(void)
-{
-    glutMainLoop();
-}
