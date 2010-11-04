3ab33a2d693a6c7d220dcd48bfa8f80d8e007c59
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-11-04 19:56:02 +0000

    Objeto de câmera para o jogo, com método adicionado para posicionar a câmera no ambiente

--- trunk/20102/pg/teste02/GPGame.h
+++ trunk/20102/pg/teste02/GPGame.h
@@ -1,14 +1,23 @@
+#ifndef GPGame_h
+#define GPGame_h
+
+class GPCamera;
+
 class GPGame
 {
     private:
-    static GPGame *instance;
+    static GPGame* instance;
     static int WIDTH;
     static int HEIGHT;
-    static const char *NAME;
+    static const char* NAME;
+    GPCamera* camera;
     GPGame();
     public:
     static void display(void);
     static void reshape(int,int);
     static GPGame* getInstance(void);
     void run(void);
+    GPCamera* getCamera(void);
 };
+
+#endif
--- trunk/20102/pg/teste02/main.cpp
+++ trunk/20102/pg/teste02/main.cpp
@@ -9,7 +9,7 @@ using namespace std;
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
-    GPGame *g = GPGame::getInstance();
-    g->run();
+    GPGame* game = GPGame::getInstance();
+    game->run();
     return EXIT_SUCCESS;
 }
