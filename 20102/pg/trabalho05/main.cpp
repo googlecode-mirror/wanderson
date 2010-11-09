5d48e19a725e88183d511c46e28ebf9ecea37af8
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-11-09 15:58:00 +0000

    Transferência da inclusão de objetos para a execução principal, comprovando abstração da ferramenta.

--- 20102/pg/trabalho05/main.cpp
+++ 20102/pg/trabalho05/main.cpp
@@ -4,6 +4,15 @@
 int main(int argc, char **argv)
 {
     Game* game = Game::getInstance();
+
+    Floor* floor = new Floor();
+    floor->setPositionY(-1);
+    game->getObjects()->add(floor);
+
+    Cube* cube = new Cube();
+    cube->setPositionX(5);
+    game->getObjects()->add(cube);
+
     game->run(&argc,argv);
     return EXIT_SUCCESS;
 }
