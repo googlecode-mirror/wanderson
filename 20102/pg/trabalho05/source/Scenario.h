e74b6712afb24233d8dcee7c3fb59013d4875b1d
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-11-30 15:05:07 +0000

    Paredes do cenário buscando desenhos com formas que identificamos os limites e vértices

--- 20102/pg/trabalho05/source/Scenario.h
+++ 20102/pg/trabalho05/source/Scenario.h
@@ -7,9 +7,12 @@
 #define SCENARIO_H_
 
 #include "Object.h"
+#include <GL/glut.h>
 
 class Scenario : public Object
 {
+private:
+    Scenario* wall(void);
 public:
     Scenario(void);
     Scenario* draw(void);
