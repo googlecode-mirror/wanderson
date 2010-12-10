2d6dc6012bfdf338aac99df823a6ae18bdd884fd
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-12-10 16:07:15 +0000

    Estrutura inicial do aplicativo. Inclusão de arquivo que inicializa o sistema e deve receber todas as requisições. Arquivo de configuração do aplicativo com dois ambientes de execução: produção e desenvolvimento. Controladora e método padrões desenvolvidos e documentados, juntamente com a camada de visualização específica.

---
+++ Blog/application/Bootstrap.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Inicializador do Sistema
+ * 
+ * Configurações necessárias para que o sistema seja executado corretamente. Se
+ * estes dados geram algum erro, provavelmente o sistema não deve ser
+ * inicializado e devemos atirar exceções para tratamento superior.
+ * 
+ * @author Wanderson Henrique Camargo Rosa
+ * @package Application
+ * @subpackage Bootstrap
+ */
+class Application_Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    
+}
