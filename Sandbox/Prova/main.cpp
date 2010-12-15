2e5ef624028026930be1eed41b1f6d702d224bb4
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-12-15 22:33:34 +0000

    Inicialização da linha de desenvolvimento principal

---
+++ Sandbox/Prova/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+struct compromisso
+{
+    char qualcompromisso[20];
+    int dia, mes;
+}
+
+class agenda
+{
+protected:
+    struct compromisso *vetC;
+    int totComp;
+public:
+    agenda(int tam)
+    {
+        vetC = new struct compromisso[tam];
+        totcomp = 0;
+    }
+    void mostratodoscomp();
+    void inserenovocomp(char qual[20], int d, int m);
+}
+
+class novaagenda: public agenda
+{
+    
+}
