bd55f196a8326720785bc64d27416d2f80029e1c
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-12-15 19:06:06 +0000

    Atualização de Bootstrap para receber plugins de controladoras e rotas adicionais para login. Modificação do menu para a controladora de autenticação, criada com formulário específico, buscando informações da tabela de usuários do banco de dados.

--- trunk/Blog/application/modules/admin/Bootstrap.php
+++ trunk/Blog/application/modules/admin/Bootstrap.php
@@ -14,6 +14,15 @@ class Admin_Bootstrap extends Zend_Application_Module_Bootstrap
     {
         /* @var $front Zend_Controller_Front */
         $front = $this->getApplication()->getResource('FrontController');
-        $front->registerPlugin(new Admin_Plugin_Layout());
+        $front->registerPlugin(new Admin_Plugin_Controller());
+    }
+
+    public function _initRouters()
+    {
+        $front = $this->getApplication()->getResource('FrontController');
+        $route = new Zend_Controller_Router_Route_Static('login', array(
+            'module' => 'admin', 'controller' => 'auth', 'action' => 'login'
+        ));
+        $front->getRouter()->addRoute('login', $route);
     }
 }
