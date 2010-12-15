a65a81270d237a70287f87ea35bb69139a434d76
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-12-15 22:41:32 +0000

    Algumas inicializações no projeto para armazenar as pessoas

--- trunk/Sandbox/Prova/main.cpp
+++ trunk/Sandbox/Prova/main.cpp
@@ -23,7 +23,24 @@ public:
     void inserenovocomp(char qual[20], int d, int m);
 }
 
+struct pessoa
+{
+    char nome[42];
+}
+
 class novaagenda: public agenda
 {
-    
+protected:
+    struct pessoa *lista;
+public:
+    novaagenda(int tam)
+    {
+        agenda::agenda(tam);
+        lista = new struct pessoa[tam];
+    }
+}
+
+int main()
+{
+    return 0;
 }
