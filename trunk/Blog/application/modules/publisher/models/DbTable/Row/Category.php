4ce568ba679a820cf2196339241739b5ac17fb20
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-12-16 19:26:13 +0000

    Módulo de publicação de artigos. Necessidade de término da programação.

---
+++ trunk/Blog/application/modules/publisher/models/DbTable/Row/Category.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Linha da Tabela de Categorias
+ * 
+ * @author Wanderson Henrique Camargo Rosa
+ * @category Publisher
+ * @package Publisher_Model
+ * @subpackage DbTable
+ */
+class Publisher_Model_DbTable_Row_Category extends Zend_Db_Table_Row_Abstract
+{
+    
+}
