48e59968ba8b0d0074a3a8912024823ee0b5501f
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2010-12-21 23:17:01 +0000

    Gerenciador de documentos com tradutor de descrição de referências bibliográficas formato BibTeX-like

--- trunk/Hazel/Bibliography/DocumentAbstract.php
+++ trunk/Hazel/Bibliography/DocumentAbstract.php
@@ -10,5 +10,33 @@
  */
 abstract class Hazel_Bibliography_DocumentAbstract
 {
-    
+    /**
+     * Nome Identificador do Documento
+     * 
+     * @var string
+     */
+    protected $_name;
+
+    /**
+     * Configuração do Nome Identificador do Documento
+     * 
+     * @param string $name
+     * @return Hazel_Bibliography_DocumentAbstract Próprio Objeto
+     */
+    public final function setName($name)
+    {
+        $name = (string) $name;
+        $this->_name = $name;
+        return $this;
+    }
+
+    /**
+     * Informação do Nome Identificador do Documento
+     * 
+     * @return string Nome Identificador para Gerenciamento
+     */
+    public final function getName()
+    {
+        return $this->_name;
+    }
 }
