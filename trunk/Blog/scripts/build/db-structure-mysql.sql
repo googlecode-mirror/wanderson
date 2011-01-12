8ee600a88b575b6d19b61e0def9594ba4d93ef75
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-01-12 16:39:12 +0000

    Atualização de estruturas do banco de dados.

--- trunk/Blog/scripts/build/db-structure-mysql.sql
+++ trunk/Blog/scripts/build/db-structure-mysql.sql
@@ -1,6 +1,6 @@
 CREATE TABLE people
 (
-    idpeople INTEGER AUTO_INCREMENT NOT NULL,
+    idpeople INTEGER UNSIGNED AUTO_INCREMENT NOT NULL,
     name VARCHAR(100),
     username VARCHAR(20) NOT NULL UNIQUE,
     password CHAR(32) NOT NULL,
@@ -10,4 +10,19 @@ CREATE TABLE people
     removed BOOLEAN NOT NULL DEFAULT FALSE,
     PRIMARY KEY(idpeople),
     CHECK(role >= 0)
+) TYPE=INNODB;
+
+CREATE TABLE message
+(
+    idmessage BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    priority SMALLINT NOT NULL DEFAULT 6,
+    priorityName VARCHAR(20),
+    content TEXT NOT NULL,
+    info TEXT,
+    ip VARCHAR(17),
+    idpeople INTEGER UNSIGNED,
+    PRIMARY KEY(idmessage),
+    FOREIGN KEY(idpeople) REFERENCES people(idpeople),
+    CHECK(priority >= 0)
 ) TYPE=INNODB;
