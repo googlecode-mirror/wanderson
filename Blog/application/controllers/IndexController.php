44cfc392dc921ec47e59e50af224900cf4d562b2
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-01-13 12:45:37 +0000

    Tratamento para falta de módulo de reescrita no servidor Web.

--- Blog/application/controllers/IndexController.php
+++ Blog/application/controllers/IndexController.php
@@ -1,9 +1,25 @@
 <?php
 
+/**
+ * Controladora Principal do Aplicativo
+ * 
+ * @author     Wanderson Henrique Camargo Rosa
+ * @category   Blog
+ * @package    Blog_Controller
+ * @subpackage Action
+ */
 class IndexController extends Zend_Controller_Action
 {
+    /**
+     * Ação Principal do Aplicativo
+     * @return void
+     */
     public function indexAction()
     {
-        
+        $uri    = $this->getRequest()->getServer('REQUEST_URI');
+        $script = $this->getRequest()->getServer('SCRIPT_NAME');
+        if (strpos($uri, $script) === false) {
+            $this->_redirect('/index.php');
+        }
     }
 }
