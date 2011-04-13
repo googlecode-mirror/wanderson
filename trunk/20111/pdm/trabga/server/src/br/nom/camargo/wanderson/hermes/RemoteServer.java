108d0796a0f7a14770eb148883660c7b244ecb05
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-04-13 12:46:43 +0000

    Encapsulamento do adaptador de dados

---
+++ trunk/20111/pdm/trabga/server/src/br/nom/camargo/wanderson/hermes/RemoteServer.java
@@ -0,0 +1,41 @@
+package br.nom.camargo.wanderson.hermes;
+
+import br.nom.camargo.wanderson.hermes.adapter.ConnectionAdapter;
+
+/**
+ * Servidor de Mensagens
+ * 
+ * Serviço para transferência de mensagens entre o dispositivo móvel e a máquina
+ * local. Trabalha utilizando adaptadores que executam a lógica especializada da
+ * comunicação. Informa objeto específico das informações transferidas. Pode
+ * trabalhar como um objeto executável.
+ * 
+ * @author Wanderson Henrique Camargo Rosa
+ */
+public class RemoteServer
+{
+    /**
+     * Adaptador da Conexão
+     */
+    private ConnectionAdapter adapter;
+
+    /**
+     * Configuração do Adaptador de Conexão
+     * @param adapter Elemento para Configuração
+     * @return Próprio Objeto para Encadeamento
+     */
+    public RemoteServer setAdapter(ConnectionAdapter adapter)
+    {
+        this.adapter = adapter;
+        return this;
+    }
+
+    /**
+     * Informação do Adaptador de Conexão
+     * @return Elemento de Informação
+     */
+    public ConnectionAdapter getAdapter()
+    {
+        return this.adapter;
+    }
+}
