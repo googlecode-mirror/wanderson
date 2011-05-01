560952d394732ce56c9e1842cbb4a9b6a54686fd
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-05-01 15:09:50 +0000

    Melhoria do exemplo utilizando encadeamento e translação.

--- trunk/20111/lp/pstree/index.php
+++ trunk/20111/lp/pstree/index.php
@@ -5,12 +5,12 @@ require_once 'tree.php';
 require_once 'psnode.php';
 require_once 'pstree.php';
 
-$tree = new PSTree();
-$tree->insert(4)
-     ->insert(2)->insert(1)->insert(3)
-     ->insert(6)->insert(5)->insert(7);
-
 PSNode::setDistanceX(25);
 PSNode::setDistanceY(25);
 
-$tree->render();
+$tree = new PSTree();
+$tree->insert(4)
+     ->insert(2)->insert(1)->insert(3)
+     ->insert(6)->insert(5)->insert(7)
+     ->setTranslateX(200)->setTranslateY(200)
+     ->render();
