05658eb82114cc59f6fd57daad8e759ad2972203
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-05-01 15:07:51 +0000

    Inicialização de alguns atributos e melhoria do erro para conteúdos idênticos

--- trunk/20111/lp/pstree/pstree.php
+++ trunk/20111/lp/pstree/pstree.php
@@ -7,6 +7,58 @@
  */
 class PSTree extends Tree
 {
+    /**
+     * Translação no Eixo das Abscissas
+     * @var float
+     */
+    protected $_translatex = 0.0;
+
+    /**
+     * Translação no Eixo das Ordenadas
+     * @var float
+     */
+    protected $_translatey = 0.0;
+
+    /**
+     * Configura a Translação no Eixo das Abcissas
+     * @param float $translate Valor para Configuração
+     * @return PSTree Próprio Objeto para Encadeamento
+     */
+    public function setTranslateX($translate)
+    {
+        $this->_translatex = (float) $translate;
+        return $this;
+    }
+
+    /**
+     * Informa a Translação no Eixo das Abcissas
+     * @return float Valor Solicitado
+     */
+    public function getTranslateX()
+    {
+        return $this->_translatex;
+    }
+
+    /**
+     * Configura a Translação no Eixo das Ordenadas
+     * @param float $translate Valor para Configuração
+     * @return PSTree Próprio Objeto para Encadeamento
+     */
+    public function setTranslateY($translate)
+    {
+        $this->_translatey = (float) $translate;
+        return $this;
+    }
+
+    /**
+     * Informa a Translação no Eixo das Ordenadas
+     * @return float Valor Solicitado
+     */
+    public function getTranslateY()
+    {
+        return $this->_translatey;
+    }
+
     public function insert($content)
     {
         $node = new PSNode($content);
@@ -20,8 +72,11 @@ class PSTree extends Tree
      */
     public function render()
     {
+        /* Translação */
+        $translatex = $this->getTranslateX();
+        $translatey = $this->getTranslateY();
         echo "%!\n";
-        echo "200 200 translate\n";
+        echo "$translatex $translatey translate\n";
         echo "/Courier findfont 10 scalefont setfont\n";
         $root = $this->getRoot();
         if ($root !== NULL) {
