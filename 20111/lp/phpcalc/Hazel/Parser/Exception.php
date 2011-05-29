ca51a08839ce40b899a2220fb771e489b86be9f4
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-05-29 01:06:39 +0000

    Inclusão da exceção do pacote para tratamento de erros

---
+++ 20111/lp/phpcalc/Hazel/Parser/Exception.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Hazel\Parser;
+
+use Exception as SplException;
+
+/**
+ * Exceção do Pacote
+ * 
+ * @author   Wanderson Henrique Camargo Rosa
+ * @category Hazel
+ * @package  Hazel\Parser
+ */
+class Exception extends SplException {}
