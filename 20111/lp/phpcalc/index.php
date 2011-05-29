73fac411b4da2cf8165a3931cd1bec149337740a
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-05-29 01:54:07 +0000

    Casos de teste para análise léxica básica.

--- 20111/lp/phpcalc/index.php
+++ 20111/lp/phpcalc/index.php
@@ -1,2 +1,43 @@
 <?php
 
+error_reporting(E_ALL|E_STRICT);
+ini_set('display_errors', true);
+
+/* Carregamento de Classes -------------------------------------------------- */
+
+function __autoload($classname) {
+    $filename = implode(DIRECTORY_SEPARATOR, array(
+        str_replace('\\', '/', $classname) . '.php'
+    ));
+    if (!is_readable($filename)) {
+        throw new ErrorException("File Not Found: '$classname'");
+    }
+    require_once $filename;
+    if (!class_exists($classname)) {
+        throw new ErrorException("Class Not Found: '$classname'");
+    }
+}
+
+/* Testes ------------------------------------------------------------------- */
+
+class CalculatorLexerTest extends PHPUnit_Framework_TestCase
+{
+    public function testTypes()
+    {
+        $lexer = new Hazel\Calculator\Lexer();
+        $lexer->read('1+');
+        /* Saída */
+        $tokenA = $lexer->getToken();
+        $tokenB = $lexer->getToken();
+        $tokenC = $lexer->getToken();
+        /* Afirmações */
+        /* Token A */
+        $this->assertTrue($tokenA->isA('T_NUMBER'));
+        $this->assertFalse($tokenA->isA('T_OPERATOR'));
+        /* Token B */
+        $this->assertTrue($tokenB->isA('T_OPERATOR'));
+        $this->assertFalse($tokenB->isA('T_NUMBER'));
+        /* Token C */
+        $this->assertSame(null, $tokenC);
+    }
+}
