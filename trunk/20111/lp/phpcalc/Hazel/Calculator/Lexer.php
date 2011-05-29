43f1f28f25ffbca8c92f3cea83468e0fd011b74c
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-05-29 01:11:00 +0000

    Análise léxica com leitura de caracter único para cada elemento na entrada.

---
+++ trunk/20111/lp/phpcalc/Hazel/Calculator/Lexer.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Hazel\Calculator;
+
+use Hazel\Parser\Exception;
+use Hazel\Parser\Lexer as LexerAbstract;
+use Hazel\Parser\Token;
+
+/**
+ * Analisador Léxico para Calculadora
+ * 
+ * @author   Wanderson Henrique Camargo Rosa
+ * @category Hazel
+ * @package  Hazel\Calculator
+ */
+class Lexer extends LexerAbstract
+{
+    /* Buffer de Caracter Único */
+    public function read($input)
+    {
+        /* Casting */
+        $input = (string) $input;
+        /* Tamanho da Entrada */
+        $length = strlen($input);
+        /* Leitura */
+        for ($i = 0; $i < $length; $i++) {
+            /* Elemento Atual */
+            $content = $input[$i];
+            if (preg_match('/^[0-9]$/', $content)) {
+                /* Número */
+                $token = new Token('T_NUMBER', $content);
+                $this->addToken($token);
+            } elseif (preg_match('/^[+]$/', $content)) {
+                /* Operador */
+                $token = new Token('T_OPERATOR', $content);
+                $this->addToken($token);
+            } else {
+                throw new Exception("Invalid Input: '$content'");
+            }
+        }
+        return $this;
+    }
+}
