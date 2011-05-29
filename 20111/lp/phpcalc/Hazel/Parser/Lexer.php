7ac80ac34bf3c2efce28c0b41df6b31da8c10d5e
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-05-29 00:50:51 +0000

    Classe abstrata para análise léxica.

---
+++ 20111/lp/phpcalc/Hazel/Parser/Lexer.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Hazel\Parser;
+
+use Hazel\Parser\Token;
+
+/**
+ * Analisador Léxico
+ * 
+ * @author   Wanderson Henrique Camargo Rosa
+ * @category Hazel
+ * @package  Hazel\Parser
+ */
+abstract class Lexer
+{
+    /**
+     * Lista de Tokens
+     * @var array
+     */
+    protected $_tokenList = array();
+
+    /**
+     * Adiciona um Token na Lista
+     * @param Token $token Elemento para Inclusão
+     * @return Lexer Próprio Objeto para Encadeamento
+     */
+    public function addToken(Token $token)
+    {
+        $this->_tokenList[] = $token;
+        return $this;
+    }
+
+    /**
+     * Informa o Token Atual
+     * @return Token Elemento Solicitado ou Nulo Conforme Ponteiro Interno
+     */
+    public function getToken()
+    {
+        list($position, $token) = each($this->_tokenList);
+        return $token;
+    }
+
+    /**
+     * Reinicialização do Ponteiro Interno
+     * @return Lexer Próprio Objeto para Encadeamento
+     */
+    public function rewind()
+    {
+        reset($this->_tokenList);
+        return $this;
+    }
+
+    /**
+     * Leitura e Análise Léxica da Entrada
+     * @param string $input Conteúdo para Análise
+     * @return Lexer Próprio Objeto para Encadeamento
+     */
+    abstract public function read($input);
+}
