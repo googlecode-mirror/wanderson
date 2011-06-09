ac211398d0e7210f72c4a540eac21082cd916805
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-09 05:06:48 +0000

    Estrutura básica do banco de dados que deve ser verificada com mais calma antes de inicializar o desenvolvimento.

---
+++ trunk/20111/lp/trabga/scripts/build/database.sql
@@ -0,0 +1,95 @@
+-- Esquema de Dados
+CREATE SCHEMA sistema;
+
+-- Instituição Origem
+CREATE TABLE 'sistema'.instituicao (
+    idinstituicao SERIAL,
+    endereco TEXT NOT NULL,
+    PRIMARY KEY(idinstituicao)
+);
+
+-- Autoria de Documentos
+CREATE TABLE 'sistema'.autor (
+    idautor SERIAL,
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    idinstituicao INT NOT NULL,
+    PRIMARY KEY(idautor),
+    FOREIGN KEY(idinstituicao) REFERENCES 'sistema'.instituicao
+        ON UPDATE CASCADE ON DELETE RESTRICT
+);
+
+-- Autor Usuário do Sistema
+CREATE TABLE 'sistema'.usuario (
+    idusuario INT NOT NULL,
+    identidade VARCHAR(20) NOT NULL UNIQUE,
+    credencial CHAR(32),
+    ativado BOOLEAN DEFAULT TRUE NOT NULL,
+    PRIMARY KEY(idusuario),
+    FOREIGN KEY(idusuario) REFERENCES 'sistema'.autor(idautor)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Artigos
+CREATE TABLE 'sistema'.artigo (
+    idartigo BIGSERIAL,
+    titulo VARCHAR(100) NOT NULL,
+    conteudo TEXT,
+    idusuario INT NOT NULL,
+    PRIMARY KEY(idartigo),
+    FOREIGN KEY(idusuario) REFERENCES 'sistema'.usuario(idusuario)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Autores de Artigos
+CREATE TABLE 'sistema'.r_artigo_autor (
+    idartigo BIGINT NOT NULL,
+    idautor INT NOT NULL,
+    PRIMARY KEY(idartigo, idautor),
+    FOREIGN KEY(idartigo) REFERENCES 'sistema'.artigo(idartigo)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY(idautor) REFERENCES 'sistema'.autor(idautor),
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Elementos para Arquivos
+CREATE TYPE REFERENCIA AS ENUM('artigo');
+
+-- Referências Bibliográficas
+CREATE TABLE 'sistema'.referencia (
+    idautor BIGSERIAL,
+    tipo REFERENCIA NOT NULL,
+    identificador VARCHAR(100) NOT NULL,
+    conteudo TEXT
+);
+
+-- Referências para Artigos
+CREATE TABLE 'sistema'.r_artigo_referencia (
+    idartigo BIGINT NOT NULL,
+    idreferencia BIGINT NOT NULL,
+    PRIMARY KEY(idartigo, idreferencia),
+    FOREIGN KEY(idartigo) REFERENCES 'sistema'.artigo(idartigo)
+        ON UPDATE CASCADE ON DELETE CASCADE,
+    FOREIGN KEY(idreferencia) REFERENCES 'sistema'.referencia(idreferencia)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
+
+-- Imagens de Documentos
+CREATE TABLE 'sistema'.figura (
+    idfigura BIGSERIAL,
+    legenda VARCHAR(100),
+    arquivo VARCHAR(100) NOT NULL,
+    identificador VARCHAR(100) NOT NULL,
+    PRIMARY KEY(idfigura)
+);
+
+-- Figuras para Artigos
+CREATE TABLE 'sistema'.r_artigo_figura (
+    idartigo BIGINT NOT NULL,
+    idfigura BIGINT NOT NULL,
+    PRIMARY KEY(idartigo, idfigura),
+    FOREIGN KEY(idartigo) REFERENCES 'sistema'.artigo(idartigo)
+        ON UPDATE CASCADE ON DELETE RESTRICT,
+    FOREIGN KEY(idfigura) REFERENCES 'sistema'.figura(idfigura)
+        ON UPDATE CASCADE ON DELETE CASCADE
+);
