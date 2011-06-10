2441815cf82678bd96418961c7dbbfb959578221
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-10 03:38:14 +0000

    Layout habilitado com algumas inclusões de código para melhorar a renderização da visualização.

--- 20111/lp/trabga/application/Bootstrap.php
+++ 20111/lp/trabga/application/Bootstrap.php
@@ -6,4 +6,23 @@
  * @category Application
  * @package  Application_Bootstrap
  */
-class Bootstrap extends Zend_Application_Bootstrap_Bootstrap {}
+class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
+{
+    /**
+     * Inicialização de Auxiliares da Visualização
+     * @return null
+     */
+    protected function _initViewHelpers()
+    {
+        $this->bootstrap('layout');
+        $layout = $this->getResource('layout');
+
+        /* @var $view Zend_View */
+        $view = $layout->getView();
+        // Doctype
+        $view->doctype(Zend_View_Helper_Doctype::XHTML1_STRICT);
+        // Meta
+        $view->headMeta()
+             ->appendHttpEquiv('Content-Type', 'text/html; charset=utf-8');
+    }
+}
