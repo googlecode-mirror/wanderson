daf3a7d8bbdef4a1f9b76f2b719aa98ca03c5777
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-11 13:11:31 +0000

    Formulário de autor modificado para acessar a tabela de autores com somente o nome e não o objeto. Criação de formulário de usuário que possui um formulário aninhado para reaproveitamento de código do formulário de autor.

---
+++ trunk/20111/lp/trabga/application/forms/Usuario.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Usuário Form
+ * 
+ * @category Application
+ * @package  Application_Form
+ */
+class Application_Form_Usuario extends Local_Form_FormAbstract
+{
+    public function init()
+    {
+        // Identidade
+        $identidade = new Zend_Dojo_Form_Element_TextBox('identidade');
+        $identidade->setLabel('Usuário')
+                   ->setDescription('Nome do Usuário para Acessar o Sistema');
+        $this->addElement($identidade);
+
+        // Credenciais
+        $credencial = new Zend_Dojo_Form_Element_PasswordTextBox('credencial');
+        $credencial->setLabel('Senha')
+                   ->setDescription('Credenciais para Autenticação');
+        $this->addElement($identidade);
+
+        // Autor
+        $autor = new Application_Form_Autor();
+        $autor->removeElement('submit');
+        $this->addSubForm($autor, 'autor');
+
+        // Botão de Envio
+        $submit = new Local_Form_Element_SubmitButton('submit');
+        $this->addElement($submit);
+    }
+}
