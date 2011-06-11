83cfa28e1c8eedf0418c4774634e6e6cd936e798
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-11 22:14:28 +0000

    Os arquivos inseridos devem pertencer aos usuários que podem acessar o sistema e não aos autores que nem sempre podem fazer este mesmo recurso.

--- trunk/20111/lp/trabga/scripts/build/database.sql
+++ trunk/20111/lp/trabga/scripts/build/database.sql
@@ -1,5 +1,7 @@
 -- Esquema de Dados
 CREATE SCHEMA sistema;
+-- Elementos para Tipos de Referências
+CREATE TYPE "sistema".TIPO_REFERENCIA AS ENUM('artigo');
 
 -- Instituição Origem
 CREATE TABLE "sistema".instituicao (
@@ -52,18 +54,15 @@ CREATE TABLE "sistema".r_artigo_autor (
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
--- Elementos para Arquivos
-CREATE TYPE REFERENCIA AS ENUM('artigo');
-
 -- Referências Bibliográficas
 CREATE TABLE "sistema".referencia (
     idreferencia BIGSERIAL,
-    tipo REFERENCIA NOT NULL,
+    tipo "sistema".TIPO_REFERENCIA NOT NULL,
     identificador VARCHAR(100) NOT NULL,
     conteudo TEXT,
-    idautor INT NOT NULL,
+    idusuario INT NOT NULL,
     PRIMARY KEY(idreferencia),
-    FOREIGN KEY(idautor) REFERENCES "sistema".autor(idautor)
+    FOREIGN KEY(idusuario) REFERENCES "sistema".usuario(idusuario)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
 
@@ -73,8 +72,8 @@ CREATE TABLE "sistema".figura (
     legenda VARCHAR(100),
     arquivo VARCHAR(100) NOT NULL,
     identificador VARCHAR(100) NOT NULL,
-    idautor INT NOT NULL,
+    idusuario INT NOT NULL,
     PRIMARY KEY(idfigura),
-    FOREIGN KEY(idautor) REFERENCES "sistema".autor(idautor)
+    FOREIGN KEY(idusuario) REFERENCES "sistema".usuario(idusuario)
         ON UPDATE CASCADE ON DELETE CASCADE
 );
