9918c9af4d189e576abc4eeff3b5fc301bb73fb1
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-11 19:17:52 +0000

    Artigos podem ser editados.

--- trunk/20111/lp/trabga/application/controllers/ArtigoController.php
+++ trunk/20111/lp/trabga/application/controllers/ArtigoController.php
@@ -25,7 +25,17 @@ class ArtigoController extends Local_Controller_ActionAbstract
      */
     public function indexAction()
     {
-        
+        // Banco de Dados
+        $table  = $this->_getDbTable();
+        $select = $table->select()->order('idartigo');
+        $result = $table->fetchAll($select);
+
+        // Mensagens
+        $messages = $this->_helper->flashMessenger->getMessages();
+
+        // Camada de Visualização
+        $this->view->result = $result;
+        $this->view->messages = $messages;
     }
 
     /**
@@ -50,8 +60,56 @@ class ArtigoController extends Local_Controller_ActionAbstract
                 $element->save();
 
                 $this->_helper->flashMessenger('insert');
-                $this->_helper->redirector('index');
+                $this->_helper->redirector('edit', null, null, array(
+                    'idartigo' => $element->idartigo
+                ));
             }
+        }
+
+        // Camada de Visualização
+        $this->view->form = $form;
+    }
+
+    /**
+     * Edit Action
+     */
+    public function editAction()
+    {
+        // Chave Primária
+        $primaries = $this->_getPrimaries(function($value){
+            return (int) $value;
+        });
+
+        // Banco de Dados
+        $table   = $this->_getDbTable();
+        $element = $table->find($primaries)->current();
+
+        // Verificação
+        if ($element === null) {
+            throw new Zend_Db_Exception('Invalid Element');
+        }
+
+        // Formulário
+        $form = $this->_getForm();
+
+        if ($this->getRequest()->isPost()) {
+            // Envio de Dados
+            $data = $this->getRequest()->getPost();
+            if ($form->isValid($data)) {
+                $data = $form->getValues();
+
+                $element->titulo = $data['cabecalho']['titulo'];
+                $element->conteudo = $data['conteudo'];
+                $element->save();
+
+                $this->_helper->flashMessenger('update');
+                $this->_helper->redirector('index');
+
+            }
+        } else {
+            // População
+            $form->cabecalho->titulo->setValue($element->titulo);
+            $form->conteudo->setValue($element->conteudo);
         }
 
         // Camada de Visualização
