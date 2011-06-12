db0bd30106ada4a5b25650adcb15336f3e97feee
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-12 18:36:10 +0000

    Início da montagem do menu de documentos utilizando Ajax.

--- trunk/20111/lp/trabga/public/scripts/app.js
+++ trunk/20111/lp/trabga/public/scripts/app.js
@@ -8,7 +8,21 @@ dojo.require('dijit.layout.BorderContainer');
 dojo.require('dijit.layout.ContentPane');
 dojo.require('dijit.layout.TabContainer');
 dojo.require('dijit.layout.AccordionContainer');
+
 // Estrutura de Classe
 dojo.declare('app', null, {
-    constructor : function() {}
+    constructor : function() {
+        this.menu = new app.menu();
+    }
+});
+
+// Estrutura de Classe
+dojo.declare('app.menu', null, {
+    container : null,
+    constructor : function() {
+        // Anexos
+        this.container = dijit.byId('layout-docs');
+    },
+    load : function() {
+    }
 });
