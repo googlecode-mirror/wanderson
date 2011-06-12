ae87c7d1e6aa1139584e1f9818da8dcff085e82a
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-12 16:06:43 +0000

    Estrutura inicial de layout utilizando Dojo Toolkit e Dijit.

---
+++ 20111/lp/trabga/public/scripts/app.js
@@ -0,0 +1,14 @@
+/**
+ * Application Dojo Module
+ * @author Wanderson Henrique Camargo Rosa
+ */
+dojo.provide('app');
+// Dependências
+dojo.require('dijit.layout.BorderContainer');
+dojo.require('dijit.layout.ContentPane');
+dojo.require('dijit.layout.TabContainer');
+dojo.require('dijit.layout.AccordionContainer');
+// Estrutura de Classe
+dojo.declare('app', null, {
+    constructor : function() {}
+});
