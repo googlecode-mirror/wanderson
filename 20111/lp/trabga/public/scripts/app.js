6492667c4c43bf4af6113f81f1dcd89e9050a2c9
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-12 20:33:29 +0000

    Inclusão de ação para serviços de Ajax na controladora de artigos. Verificação de conexão do tipo Ajax em alguns pontos da controladora, evitando construção de layout ou fornecendo mensagens de aviso utilizando Json. Remoção do inicializador de sistema em Javascript do layout para o módulo em Dojo; necessidade de utilização de um nome fixo dentro dos elementos criados. Métodos para manipulação de artigos abertos no sistema.

--- 20111/lp/trabga/public/scripts/app.js
+++ 20111/lp/trabga/public/scripts/app.js
@@ -9,10 +9,24 @@ dojo.require('dijit.layout.ContentPane');
 dojo.require('dijit.layout.TabContainer');
 dojo.require('dijit.layout.AccordionContainer');
 
+dojo.require('dijit.form.Form');
+dojo.require('dijit.form.TextBox');
+dojo.require('dijit.form.Textarea');
+
 // Estrutura de Classe
 dojo.declare('app', null, {
+    menu : null,
+    tabber : null,
+    console : null,
+    document : null,
     constructor : function() {
-        this.menu = new app.menu();
+        this.menu     = new app.menu();
+        this.tabber   = new app.tabber();
+        this.console  = new app.console();
+        this.document = new app.document();
+    },
+    start : function() {
+        this.console.clear();
     }
 });
 
@@ -20,9 +34,98 @@ dojo.declare('app', null, {
 dojo.declare('app.menu', null, {
     container : null,
     constructor : function() {
+        // Painel de Documentos
+        var container = dijit.byId('layout-docs');
+        dojo.connect(container, 'onDownloadEnd', function(){
+            dojo.query('#layout-docs a').forEach(function(element){
+                dojo.connect(element, 'onclick', function(event){
+                    dojo.stopEvent(event);
+                    system.tabber.open(element.text, element.href);
+                });
+            });
+        });
+        container.setHref('artigo/service');
         // Anexos
-        this.container = dijit.byId('layout-docs');
+        this.container = container;
     },
     load : function() {
+        this.container.refresh();
     }
+});
+
+// Estrutura de Classe
+dojo.declare('app.tabber', null, {
+    container : null,
+    constructor : function() {
+        // Container de Tabs
+        var container = dijit.byId('layout-tabcontainer');
+        // Anexos
+        this.container = container;
+    },
+    // Abrir uma Nova Aba
+    open : function(title, address) {
+        // Pesquisar Aba
+        var tab = null;
+        var children = this.container.getChildren();
+        dojo.forEach(children, function(element){
+            if (element.attr('href') == address) {
+                tab = element;
+            }
+        });
+        if (tab == null) {
+            tab = new dijit.layout.ContentPane({
+                href : address,
+                title : title,
+                closable : true
+            });
+            this.container.addChild(tab);
+            dojo.connect(tab, 'onDownloadEnd', function(){
+                var forms = dojo.query(tab.domNode).query('form');
+                forms.connect('onsubmit', function(event){
+                    dojo.stopEvent(event);
+                    system.document.submit(this);
+                });
+            });
+        }
+        this.container.selectChild(tab);
+    }
+});
+
+dojo.declare('app.document', null, {
+    constructor : function() {},
+    // Envia um Artigo
+    submit : function(form) {
+        dojo.xhrPost({
+            form : form,
+            handleAs : 'json',
+            load : function(data) {
+                dojo.forEach(data.messages, function(message){
+                    system.console.print(message);
+                });
+            }
+        });
+    }
+});
+
+dojo.declare('app.console', null, {
+    container : null,
+    constructor : function() {
+        // Componente para Saída de Mensagens
+        this.container = dijit.byId('layout-output');
+    },
+    clear : function() {
+        dojo.empty(this.container.id);
+    },
+    print : function(message) {
+        var node = this.container.domNode;
+        var div  = dojo.doc.createElement('div');
+        dojo.attr(div, 'innerHTML', message);
+        dojo.place(div, node);
+    }
+});
+
+var system;
+dojo.addOnLoad(function(){
+    system = new app();
+    system.start();
 });
