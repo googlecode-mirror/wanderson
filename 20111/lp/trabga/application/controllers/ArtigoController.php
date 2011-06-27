fa2d24fc8f180e1a6a3f09f9acf4026630692131
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-27 01:25:04 +0000

    Possibilidade de renderização de dados de referências bibliográficas a cargo da camada de modelo. Controladora de artigos com ação de exportar completa, como alvo arquivo pdf único ou compactado com o código fonte gerado. Camada de visualização com a base para renderização de artigo. Gramática modificada para armazenar mais informações.

--- 20111/lp/trabga/application/controllers/ArtigoController.php
+++ 20111/lp/trabga/application/controllers/ArtigoController.php
@@ -27,6 +27,18 @@ class ArtigoController extends Local_Controller_ActionAbstract
     protected $_parser;
 
     /**
+     * Imagens Utilizadas na Exportação
+     * @var Zend_Db_Table_Rowset_Abstract
+     */
+    protected $_images = array();
+
+    /**
+     * Citações Utilizadas na Exportação
+     * @var Zend_Db_Table_Rowset_Abstract
+     */
+    protected $_citations = array();
+
+    /**
      * Index Action
      */
     public function indexAction()
@@ -94,7 +106,7 @@ class ArtigoController extends Local_Controller_ActionAbstract
         // FlashMessenger sem Nova Requisição
         $messages = array();
 
-        // Banco de Dados
+        // Banco de Dados$dest
         $table   = $this->_getDbTable();
         $element = $table->find($primaries)->current();
 
@@ -191,10 +203,13 @@ class ArtigoController extends Local_Controller_ActionAbstract
         $cts = new CommonTokenStream($lex);
 
         // Tradutor
-        $parser = new SubWikiParser($cts);
+        $parser = $this->_getParser();
+        $parser->setTokenStream($cts);
 
         // Usuário Atual
         $usuario = $element->findParentRow('Usuario');
+        $autor   = $usuario->findParentRow('Autor');
+        $instituicao = $autor->findParentRow('Instituicao');
 
         // Preenchimento de Imagens Disponíveis
         $figuras = $usuario->findDependentRowset('Figura');
@@ -204,6 +219,7 @@ class ArtigoController extends Local_Controller_ActionAbstract
             $caption    = $figura->legenda;
             $parser->addImageInfo($identifier, $filename, $caption);
         }
+        $this->_images = $figuras;
 
         // Preenchimento de Citações Disponíveis
         $referencias = $usuario->findDependentRowset('Referencia');
@@ -211,13 +227,164 @@ class ArtigoController extends Local_Controller_ActionAbstract
             $identifier = $referencia->identificador;
             $parser->addCiteInfo($identifier);
         }
+        $this->_citations = $referencias;
 
         // Processamento
         $parser->wikipage();
+        $this->view->document = trim($parser->render());
 
-        // Ignorar Renderização de Saída
+        // Pacotes
+        $this->view->packages = $this->_getPackages();
+
+        // Informações Pessoais e Artigo
+        $this->view->title  = $element->titulo;
+        $this->view->author = $autor->nome;
+        $this->view->email  = $autor->email;
+        $this->view->address = $instituicao->endereco;
+
+        // Citações Inclusas?
+        $citations = $parser->getCitations();
+        $this->view->citations = !empty($citations);
+
+        // Construção da Saída
+        $pathname = $this->_buildOutput();
+
+        // Tipo de Saída Esperada
+        $extension = $this->_getParam('type', 'pdf');
+        if (!in_array($extension, array('pdf', 'zip'))) {
+            $extension = 'pdf';
+        }
+
+        // Código Completo Compactado?
+        if ($extension == 'zip') {
+            $filter = new Zend_Filter_Compress(array(
+                'adapter' => 'Zip',
+                'options' => array(
+                    'archive' => $pathname . '/document.zip'
+                )
+            ));
+            $filter->filter($pathname);
+        }
+
+        // Capturar o Conteúdo
+        $content = file_get_contents($pathname . '/document.' . $extension);
+        // @todo Remover Diretório Temporário
+
+        // Preparação para Ignorar Renderização de Saída
         $renderer = $this->_helper->getHelper('ViewRenderer');
         $renderer->setNoRender(true);
         $this->view->layout()->disableLayout();
+
+        // Modificação de Cabeçalho
+        $this->getResponse()
+             ->setHeader('Content-Type', 'application/' . $extension)
+             ->setHeader('Content-Disposition', "attachment;filename=document.$extension")
+             ->append('content', $content);
+    }
+
+    /**
+     * Captura do Tradutor
+     * @return SubWikiParser Elemento Solicitado
+     */
+    protected function _getParser()
+    {
+        if ($this->_parser === null) {
+            $this->_parser = new SubWikiParser(null);
+        }
+        return $this->_parser;
+    }
+
+    /**
+     * Construção da Lista de Pacotes Utilizados
+     * @return array Conjunto de Elementos
+     */
+    protected function _getPackages()
+    {
+        // Pacotes Iniciais
+        $packages = array();
+        $packages['inputenc'] = array('utf8');
+        $packages['fontenc'] = array('T1');
+        $packages['babel'] = array('brazil');
+        $packages['sbc-template'] = array();
+        // Imagens Inseridas?
+        $images = $this->_getParser()->getImages();
+        if (!empty($images)) {
+            $packages['graphicx'] = array();
+        }
+        return $packages;
+    }
+
+    /**
+     * Construção da Saída Renderizada
+     * @return string Nome do Diretório com o Conteúdo Processado
+     */
+    protected function _buildOutput()
+    {
+        // Diretório para Armazenamento Temporário
+        $tempdir = APPLICATION_PATH . '/../temp/build-';
+        do {
+            $pathname = $tempdir . str_replace('.', '', microtime(true));
+        } while (is_dir($pathname));
+
+        // Tradutor
+        $parser = $this->_getParser();
+
+        // Construção da Saída
+        $output = $this->view->render('article.phtml');
+        if (!@mkdir($pathname)) { // Diretório
+            throw new Exception('Temp Directory Error');
+        }
+        $pathname = realpath($pathname);
+        file_put_contents($pathname . '/document.tex', $output);
+
+        // Imagens
+        $source = APPLICATION_PATH . '/../public/images/sistema';
+        $images = $parser->getImages();
+        foreach ($this->_images as $image) {
+            if (in_array($image->identificador, $images)) {
+                $arquivo = '/' . $image->arquivo;
+                copy($source . $arquivo, $pathname . $arquivo);
+            }
+        }
+
+        // Citações
+        $citations = $parser->getCitations();
+        if (!empty($citations)) { // Citações Inclusas?
+            $result    = '';
+            foreach ($this->_citations as $citation) {
+                if (in_array($citation->identificador, $citations)) {
+                    $result .= $citation->render();
+                }
+            }
+            file_put_contents($pathname . '/document.bib', $result);
+        }
+
+        // Compilação LaTeX
+        $prefix = "cd '$pathname' && ";
+        $this->_exec('pdflatex document', $prefix); // Leitura do Documento
+        if (!empty($citations)) {
+            $this->_exec('bibtex document', $prefix); // Bibliografia
+        }
+        $this->_exec('pdflatex document', $prefix); // Referências Cruzadas
+        $this->_exec('pdflatex document', $prefix); // Bibliografia
+
+        // Informa o Diretório Utilizado
+        return $pathname;
+    }
+
+    /**
+     * Executa Determinado Comando para Compilação
+     * @param string $command Comando para Execução
+     * @param string $prefix  Prefixo do Comando
+     * @return ArtigoController Próprio Objeto para Encadeamento
+     * @throws Exception Erro de Execução Encontrado
+     */
+    protected function _exec($command, $prefix = '')
+    {
+        exec($prefix . $command, $output, $return);
+        if ($return != 0) {
+            throw new Exception('LaTeX Compiler Error', $return);
+        }
+        return $this;
     }
 }
