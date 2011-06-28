7a871dbd4d2122428eb630fbab7d624d6756d962
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-06-28 03:48:13 +0000

    Ação de armazenamento Json utilizado pelo menu para renderizar a árvore de artigos para utilização. Configuração do banco de dados e novo layout disponível, utilizando dijit. Arquivo de inicialização de Javascript.

--- 20111/lp/trabga/public/scripts/app/app.js
+++ 20111/lp/trabga/public/scripts/app/app.js
@@ -1,2 +1,40 @@
 dojo.provide('app');
-dojo.declare('app', null, {});
+dojo.require('dijit.layout.ContentPane');
+dojo.declare('app', null, {
+    _baseUrl : '/artigos',
+    baseUrl : function(address) {
+        return this._baseUrl + address;
+    }
+});
+
+// Start System
+var sys = new app();
+
+// Ready
+dojo.ready(function(){
+
+    // Menu Lateral - Abrir Artigo
+    dojo.connect(dijit.byId('documents'), 'onClick', function(item){
+        if (!item.root) {
+            var content = null;
+            var idartigo = 'artigo' + item.idartigo;
+            var container = dijit.byId('tab_container');
+            dojo.forEach(container.getChildren(), function(element){
+                if (element.attr('id') == idartigo) {
+                    content = element;
+                }
+            });
+            if (content == null) {
+                content = new dijit.layout.ContentPane({
+                    id : idartigo,
+                    title : item.titulo,
+                    closable : true
+                });
+                container.addChild(content);
+            }
+            container.selectChild(content);
+            content.attr('href', sys.baseUrl('/artigo/edit/idartigo/'+item.idartigo));
+        }
+    });
+
+});
