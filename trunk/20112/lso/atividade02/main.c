a7e663c0f6197074ad9d728b70c9692636c2fcdd
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-09-22 01:25:18 +0000

    Atividade 02 Etapa 01

---
+++ trunk/20112/lso/atividade02/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * Atividade 02
+ * Laboratório de Sistemas Operacionais
+ * @author Wanderson Henrique Camargo Rosa
+ */
+int main(int argc, char* argv[]) {
+    // Envio para Saída Padrão
+    printf("Olá Mundo\n"); // Último Caractere Quebra de Linha
+    // Formatação de Ponto Flutuante
+    printf("%.3f", 3.141581); // Três Casas para Mantissa
+    // Retorno de Resultados ao Sistema Operacional
+    return 0; // Estado de Terminação com Sucesso
+}
