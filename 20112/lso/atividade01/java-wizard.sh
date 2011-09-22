19d7ca779e6447efa6ffb546823a0b491e301ef8
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-09-22 00:02:00 +0000

    Atividade 01 Etapa 02

--- 20112/lso/atividade01/java-wizard.sh
+++ 20112/lso/atividade01/java-wizard.sh
@@ -2,6 +2,12 @@
 # Java Wizard
 # @author Wanderson Henrique Camargo Rosa
 
+# Verificação de Parâmetro
+if [ -z "$1" ]; then # Bloco Condicional
+    echo "Erro: Nome de Classe Necessário" # Mensagem Informativa de Erro
+    exit 1 # Estado de Finalização com Falha (Diferente de Zero)
+fi
+
 # Nome da Classe
 echo "$1" # Saída Padrão
 
