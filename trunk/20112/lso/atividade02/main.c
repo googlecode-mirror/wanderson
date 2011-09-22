0989f638caf682ab7b892502eae0ad31a1efb0e7
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-09-22 01:59:23 +0000

    Atividade 02 Etapa 05

--- trunk/20112/lso/atividade02/main.c
+++ trunk/20112/lso/atividade02/main.c
@@ -4,6 +4,7 @@
 // Assinatura de Funções
 void imprime_caracteres(char*);
 void imprime_bytes_dec(char*);
+void imprime_bytes_hex(char*);
 
 /**
  * Atividade 02
@@ -19,7 +20,7 @@ int main(int argc, char* argv[]) {
     scanf("%s", input); // Armazenamento do Conteúdo
 
     // Execução de Função Encapsulada
-    imprime_bytes_dec(input);
+    imprime_bytes_hex(input);
 
     // Retorno de Resultados ao Sistema Operacional
     return 0; // Estado de Terminação com Sucesso
@@ -53,6 +54,22 @@ void imprime_bytes_dec(char* input) {
     size = strlen(input); // Necessidade de Cabeçalho
     for (int i = 0; i < size; i++) { // Laço de Repetição para Caracteres
         printf("%d\n", input[i]);
+    }
+}
+
+/**
+ * Impressão de Caracteres de Entrada
+ * Exibição dos Valores em Números Hexadecimais
+ * @param char* input Sequência de Entrada
+ * @return void
+ */
+void imprime_bytes_hex(char* input) {
+    // Contador do Tamanho da String de Entrada
+    int size; // Não Necessária Inicialização
+    // Cálculo do Tamanho da String de Entrada
+    size = strlen(input); // Necessidade de Cabeçalho
+    for (int i = 0; i < size; i++) { // Laço de Repetição para Caracteres
+        printf("%x\n", input[i]);
     }
 }
 
