67179cbc10c30da5c817dff794c9364ad3ef8cf6
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-09-22 01:46:29 +0000

    Atividade 02 Etapa 03

--- 20112/lso/atividade02/main.c
+++ 20112/lso/atividade02/main.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <string.h>
 
+// Assinatura de Funções
+void imprime_caracteres(char*);
+
 /**
  * Atividade 02
  * Laboratório de Sistemas Operacionais
@@ -10,18 +13,30 @@ int main(int argc, char* argv[]) {
     // Variáveis
     // String de Entrada
     char input[128] = ""; // Máximo 128 Caracteres
-    // Contador do Tamanho da String de Entrada
-    int size = 0;
 
     // Captura de Entrada Padrão
     scanf("%s", input); // Armazenamento do Conteúdo
+
+    // Execução de Função Encapsulada
+    imprime_caracteres(input);
+
+    // Retorno de Resultados ao Sistema Operacional
+    return 0; // Estado de Terminação com Sucesso
+}
+
+/**
+ * Impressão de Caracteres de Entrada
+ * @param char* input Sequência de Caracteres
+ * @return void
+ */
+void imprime_caracteres(char* input) {
+    // Contador do Tamanho da String de Entrada
+    int size = 0;
 
     // Cálculo do Tamanho da String de Entrada
     size = strlen(input); // Necessidade de Cabeçalho
     for (int i = 0; i < size; i++) { // Laço de Repetição para Caracteres
         printf("%c\n", input[i]); // Impressão de Caractere, um por Linha
     }
-
-    // Retorno de Resultados ao Sistema Operacional
-    return 0; // Estado de Terminação com Sucesso
 }
+
