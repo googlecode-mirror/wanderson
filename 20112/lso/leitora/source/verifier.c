358fc89d32fb9a0d47717f0f8a19a4e1fed77831
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-11-20 13:11:27 +0000

    Inicialização do registro para verificadora de CPF e CNPJ, alocando o dispositivo de caractere no sistema operacional.

--- 20112/lso/leitora/source/verifier.c
+++ 20112/lso/leitora/source/verifier.c
@@ -2,6 +2,7 @@
 #include <linux/init.h>
 #include <linux/kdev_t.h>
 #include <linux/fs.h>
+#include <linux/cdev.h>
 
 // Informações
 MODULE_DESCRIPTION("Driver para Verificadora de CPF e CNPJ");
@@ -19,16 +20,41 @@ MODULE_LICENSE("Dual BSD/CPL");
 // Assinaturas
 int verifier_init(void);
 void verifier_exit(void);
+ssize_t verifier_read(struct file*, char __user*, size_t, loff_t*);
+ssize_t verifier_write(struct file*, const char __user*, size_t, loff_t*);
+int verifier_open(struct inode*, struct file*);
+int verifier_release(struct inode*, struct file*);
 
 // Registros
 module_init(verifier_init);
 module_exit(verifier_exit);
 
 /**
+ * Número Maior do Dispositivo
+ */
+int MAJOR_NUMBER;
+
+/**
  * Dispositivo
  * Valor Representante do Elemento no Sistema
  */
 dev_t device;
+
+/**
+ * Dispositivo Alocado
+ * Posição em Memória do Dispositivo de Caractere
+ */
+struct cdev *cdevice;
+
+/**
+ * Estrutura com Operadores de Arquivos
+ */
+struct file_operations verifier_fops = {
+    .read    = verifier_read,
+    .write   = verifier_write,
+    .open    = verifier_open,
+    .release = verifier_release,
+};
 
 /**
  * Inicialização do Módulo
@@ -49,9 +75,23 @@ int verifier_init(void) {
     if (result == 0) {
         // Inicialização com Sucesso
         printk(KERN_INFO "Inicialização com Sucesso");
+        // Capturar o Número Maior
+        MAJOR_NUMBER = MAJOR(device);
+        // Alocação em Memória do Dispositivo
+        cdevice = cdev_alloc();
+        cdevice->ops = &verifier_fops;
+        result = cdev_add(cdevice, device, 1);
+        // Verificar Resultado
+        if (result == 0) {
+            // Alocação com Sucesso
+            printk(KERN_INFO "Alocação com Sucesso");
+        } else {
+            // Alocação com Erros
+            printk(KERN_ALERT "Inicialização com Problemas: Impossível Alocar Dispositivo no Sistema");
+        }
     } else {
         // Inicialização com Erros
-        printk(KERN_ALERT "Inicialização com Problemas: Impossível Alocação de Dispositivo");
+        printk(KERN_ALERT "Inicialização com Problemas: Impossível Inicializar Dispositivo");
     }
     return result;
 }
@@ -67,9 +107,83 @@ int verifier_init(void) {
 void verifier_exit(void) {
     // Finalização
     printk(KERN_INFO "Finalização de Verificadora CPF/CNPJ");
+    // Desalocar Dispositivo de Caractere
+    cdev_del(cdevice);
     // Liberar Dispositivo Inicializado
     unregister_chrdev_region(device, DEVICE_COUNTER);
     // Finalização com Sucesso
     printk(KERN_INFO "Finalização com Sucesso");
+}
+
+/**
+ * Leitura no Dispositivo
+ * 
+ * Executa uma leitura no dispositivo, apresentando informações armazenadas em
+ * memória local para o espaço de usuário utilizando ferramentas do Kernel.
+ * Basicamente, ignora as informações solicitadas e apresenta a quantidade de
+ * vezes que foi executada a leitura no dispositivo.
+ *
+ * @param filp   Ponteiro ao Arquivo
+ * @param buffer Posição de Memória em Espaço de Usuário
+ * @param count  Quantidade de Caracteres para Processamento
+ * @param offp   Posição Atual do Arquivo Acessada
+ *
+ * @return Quantidade de bytes processados com sucesso pela leitura
+ */
+ssize_t verifier_read(struct file* filp, char __user* buffer, size_t count, loff_t* offp) {
+    printk(KERN_DEBUG "Leitura em Arquivo");
+    return count;
+}
+
+/**
+ * Escrita no Dispositivo
+ * 
+ * Executa uma escrita no dispositivo, capturando informações armazenadas em
+ * espaço de usuário e copiando para memória local utilizando ferramentas do
+ * Kernel. Basicamente, contabiliza a quantidade de vezes que o dispositivo
+ * efetuou uma escrita.
+ *
+ * @param filp   Ponteiro ao Arquivo
+ * @param buffer Posição de Memória em Espaço de Usuário
+ * @param count  Quantidade de Caracteres para Processamento
+ * @param offp   Posição Atual do Arquivo Acessada
+ *
+ * @return Quantidade de bytes processados com sucesso pela escrita
+ */
+ssize_t verifier_write(struct file* filp, const char __user* buffer, size_t count, loff_t* offp) {
+    printk(KERN_DEBUG "Escrita em Arquivo");
+    return count;
+}
+
+/**
+ * Abertura de Arquivo
+ *
+ * Execução para abertura do arquivo solicitado pelo sistema operacional para o
+ * dispositivo. Neste caso, como não vamos trabalhar com nenhum dispositivo
+ * físico, somente precisamos confirmar a execução da função.
+ *
+ * @param inode Representante do Arquivo no Sistema
+ * @param filp  Ponteiro ao Representante de Arquivo Aberto no Sistema
+ *
+ * @return Confirmação de sucesso para execuções internas
+ */
+int verifier_open(struct inode* inode, struct file* filp) {
+    return 0;
+}
+
+/**
+ * Fechamento de Arquivo
+ *
+ * Execução para fechamento do arquivo solicitado pelo sistema operacional para
+ * o dispositivo. Neste caso, como não vamos trabalhar com nenhum dispositivo
+ * físico, somente precisamos confirmar a execução da função.
+ *
+ * @param inode Representante do Arquivo no Sistema
+ * @param filp  Ponteiro ao Representante de Arquivo Aberto no Sistema
+ *
+ * @return Confirmação de sucesso para execuções internas
+ */
+int verifier_release(struct inode* inode, struct file* filp) {
+    return 0;
 }
 
