d052d2f295f218caecbe79d00f33d17d619f1afa
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-12-08 19:22:20 +0000

    Primeira Interface para inclusão de produtos dentro do Carrinho de Compras.

---
+++ trunk/Hazel/Shop/Basket/ProductInterface.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Hazel Zend Framework Extended Library
+ */
+
+/**
+ * Interface de Produtos
+ *
+ * Buscando separar o pacote de Carrinho de Compras de outros projetos, esta
+ * Interface deve ser aplicada aos produtos que devem ser armazenados pela
+ * estrutura desenvolvida. Lembre-se de que os elementos adicionados também
+ * serão armazenados pela camada de persistência.
+ *
+ * @author     Wanderson Henrique Camargo Rosa <wanderson@f1solucoes.com.br>
+ * @see        http://www.wanderson.camargo.nom.br
+ * @category   Hazel
+ * @package    Hazel_Shop
+ * @subpackage Basket
+ */
+interface Hazel_Shop_Basket_ProductInterface
+{
+    /**
+     * Código do Produtos
+     *
+     * Acesso ao código do produto, identificador único que é utilizado para
+     * manipular produtos específicos dentro do Carrinho de Compras.
+     *
+     * @return string Valor Solicitado
+     */
+    public function getCode();
+
+    /**
+     * Valor do Produto
+     *
+     * Preço que deve ser utilizado pelo Carrinho de Compras como base para
+     * cálculos pelos Plugins de execução.
+     *
+     * @return float Valor Solicitado
+     */
+    public function getValue();
+
+}
