d3cdcaf695912a32c907a926dec597b2837910f3
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2011-12-09 13:14:06 +0000

    Inclusão de camada de persistência para a classe de Carrinho de Compras e elemento para tratamento de erros do pacote.

---
+++ trunk/Hazel/Shop/Basket/Exception.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Hazel Zend Framework Extended Library
+ */
+
+namespace \Hazel\Shop\Basket;
+
+/**
+ * Exceção do Pacote
+ *
+ * Classe utilizada para apresentação de erros dentro do pacote de Carrinho de
+ * Compras. Todos os erros gerados dentro do pacote devem ser derivados desta
+ * classe.
+ *
+ * @author     Wanderson Henrique Camargo Rosa <wanderson@f1solucoes.com.br>
+ * @see        http://www.wanderson.camargo.nom.br
+ * @category   Hazel
+ * @package    Hazel_Shop
+ * @subpackage Basket
+ */
+class Exception extends \Exception {}
