752daa7491a2ddf75d817949393bd5fddd8d8afd
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-02-12 13:33:05 +0000

    Auxiliar de visualização para renderizar um conteúdo em string conforme limite solicitado, concatenando um sufixo.

---
+++ Hazel/View/Helper/StringLimit.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * Limitador de Caracteres
+ *
+ * Auxiliar de visualização que busca limitar a quantidade de caracteres que
+ * devem ser exibidos durante uma renderização. Ao final, sempre é concatenado
+ * um sufixo solicitado, nunca ultrapassando o limite máximo atribuído.
+ *
+ * @category   Hazel
+ * @package    Hazel_View
+ * @subpackage Helper
+ */
+class Hazel_View_Helper_StringLimit extends Zend_View_Helper_Abstract {
+
+    /**
+     * Método Principal Limitador de Conteúdo
+     *
+     * Busca limitar a quantidade de caracteres que devem ser exibidos durante
+     * uma renderização, possibilitando assim sempre exibir a quantidade limite
+     * solicitada, preenchendo com um sufixo solicitado, ainda não ultrapassando
+     * o valor máximo.
+     *
+     * @param  string $content Conteúdo para Limitação
+     * @param  int    $limit   Quantidade Máxima Limite para Exibição
+     * @param  string $suffix  Sufixo para Preenchimento
+     * @return string Conteúdo Limitado conforme Solicitado
+     */
+    public function stringLimit($content, $limit = 255, $suffix = '...') {
+        // Conversão
+        $content = (string) $this->view->escape($content);
+        $limit   = (int) $limit;
+        $suffix  = (string) $suffix;
+        // Verificação de Limite Válido
+        if (($suffix_len = strlen($suffix)) > $limit) {
+            // Limite Máximo Menor que Tamanho do Sufixo
+            throw new Zend_View_Exception("Invalid Suffix Length: $suffix_len:$limit");
+        }
+        // Conteúdo Menor que Limite
+        if (($content_len = strlen($content)) <= $limit) {
+            return $content;
+        }
+        // Conteúdo com Tamanho do Sufixo
+        if ($content_len - $suffix_len <= 0) {
+            return $suffix;
+        }
+        // Conteúdo Maior que Limite
+        return substr($content, 0, $limit - $suffix_len) . $suffix;
+    }
+
+}
+
