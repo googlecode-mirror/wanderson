236a4cefc7d3ca2336a8c7642c48e8ba31c5fdd0
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-03-10 16:20:08 +0000

    Inclusão de programa para linhas de comando que executa inclusão de arquivos e seleção randômica conforme classe de modelo adicionada.

---
+++ trunk/Sandbox/pokeflute/config.php
@@ -0,0 +1,15 @@
+<?php
+// Configurações Iniciais
+define('APPLICATION_PATH', realpath(dirname(__FILE__) . '/application'));
+define('APPLICATION_ENV', 'development');
+// Caminho de Inclusão
+set_include_path(implode(PATH_SEPARATOR, array(
+    realpath(APPLICATION_PATH . '/../library'),
+    get_include_path(),
+)));
+// Carregamento Automático
+require_once 'Pokeflute/Autoloader.php';
+\Pokeflute\Autoloader::getInstance();
+// Configurações
+$config = \Pokeflute\Multiton::getInstance('Pokeflute\\Config');
+$config->set('music.directory', APPLICATION_PATH . '/../data/musics');
