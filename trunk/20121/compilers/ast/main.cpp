744b6f99b31ab8506aa73dbaf56e2f3ff223ace9
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-06 22:17:17 +0000

    Exemplo um pouco mais especializado para verificação de indentações corretas.

--- trunk/20121/compilers/ast/main.cpp
+++ trunk/20121/compilers/ast/main.cpp
@@ -8,7 +8,9 @@ int main()
     // Expressão
     AST::Operand  *nodel = new AST::Operand(0, "1");
     AST::Operand  *noder = new AST::Operand(0, "2");
-    AST::Operator *node  = new AST::Operator(1, "+", nodel, noder);
+    AST::Operand  *nodeu = new AST::Operand(0, "3");
+    AST::Operator *plus  = new AST::Operator(1, "+", nodel, noder);
+    AST::Operator *node  = new AST::Operator(2, "*", nodeu, plus);
     // Árvore
     AST::Tree *tree = new AST::Tree(node);
     // Renderização
