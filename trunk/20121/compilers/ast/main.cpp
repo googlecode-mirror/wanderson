c91ac281ecb789ce7c572e760806a0d28fbe4681
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-06 20:54:19 +0000

    Inclusão de estrutura para utilização de operadores matemáticos, incluindo referências para expressão à esquerda e direita

--- trunk/20121/compilers/ast/main.cpp
+++ trunk/20121/compilers/ast/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include "operand.h"
+#include "operator.h"
 using namespace std;
 int main()
 {
-    AST::Operand *node = new AST::Operand(0, "+");
+    AST::Operand *nodel = new AST::Operand(0, "1");
+    AST::Operand *noder = new AST::Operand(0, "2");
+    AST::Operator *node = new AST::Operator(1, "+", nodel, noder);
     cout << node->toString() << endl;
     return 0;
 }
