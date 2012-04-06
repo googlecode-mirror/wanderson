6c0880174623c0ae9ea0bce626fe2b5bc58c8e8f
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-06 21:51:09 +0000

    Estruturação de comentários melhorados em nó básico de árvore. Modificações no Makefile para suportar automaticamente a compilação de elementos básicos

--- 20121/compilers/ast/main.cpp
+++ 20121/compilers/ast/main.cpp
@@ -1,12 +1,17 @@
 #include <iostream>
 #include "operand.h"
 #include "operator.h"
+#include "tree.h"
 using namespace std;
 int main()
 {
-    AST::Operand *nodel = new AST::Operand(0, "1");
-    AST::Operand *noder = new AST::Operand(0, "2");
-    AST::Operator *node = new AST::Operator(1, "+", nodel, noder);
-    cout << node->toString() << endl;
+    // Expressão
+    AST::Operand  *nodel = new AST::Operand(0, "1");
+    AST::Operand  *noder = new AST::Operand(0, "2");
+    AST::Operator *node  = new AST::Operator(1, "+", nodel, noder);
+    // Árvore
+    AST::Tree *tree = new AST::Tree(node);
+    // Renderização
+    cout << tree->toString() << endl;
     return 0;
 }
