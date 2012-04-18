6313270c230f9a2d378ac24cee4761a3690b2b52
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-18 02:08:31 +0000

    Inclusão de coluna representante de usuário tipo administrativo, capaz de gerenciar outros usuários dentro do sistema

--- 20121/dw/wsl/scripts/database/users.sql
+++ 20121/dw/wsl/scripts/database/users.sql
@@ -7,14 +7,15 @@ CREATE TABLE IF NOT EXISTS `wsl_users`(
     `id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
     `hash` CHAR(40) NOT NULL UNIQUE,
     `email` VARCHAR(255) NOT NULL UNIQUE,
-    `active` TINYINT NOT NULL DEFAULT 1,
+    `active` TINYINT UNSIGNED NOT NULL DEFAULT 1,
+    `admin` TINYINT UNSIGNED NOT NULL DEFAULT 0,
     PRIMARY KEY(`id`)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
 COLLATE = utf8_unicode_ci;
 
-INSERT INTO `users`(`hash`,`email`) VALUES
-    ('7c4a8d09ca3762af61e59520943dc26494f8941b', 'root@localhost');
+INSERT INTO `users`(`hash`,`email`,`admin`) VALUES
+    ('7c4a8d09ca3762af61e59520943dc26494f8941b', 'root@localhost', 1);
 -- end
 
