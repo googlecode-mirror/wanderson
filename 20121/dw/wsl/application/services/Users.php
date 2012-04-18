429ace3508e2ea59cc79e5b65a71054d4e7bfe87
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-18 13:40:58 +0000

    Inclusão de exemplo para serviço utilizado com parâmetros de documentação que devem ser prococessados pela classe de construção de WSDL.

---
+++ 20121/dw/wsl/application/services/Users.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Serviço de Usuários
+ *
+ * @category Application
+ * @package  Application_Service
+ */
+class Service_Users {
+
+    /**
+     * Salvar Usuário
+     *
+     * @request  int      $id    Identificador do Usuário
+     * @request  string   $email E-mail para Configuração
+     * @response int      $id    Identificador do Usuário
+     * @response string   $hash  Código Hash do Usuário Atual
+     * @response string   $email E-mail Configurado
+     * @return   string[] Confirmação de Salvamento
+     */
+    public function saveAction() {
+        // Apresentação
+        return array(
+            'id'    => 1,
+            'hash'  => '123456',
+            'email' => 'wandersonwhcr@gmail.com',
+        );
+    }
+
+}
+
