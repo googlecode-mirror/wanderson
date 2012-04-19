da6e1033602b44ee67e980d6e0be91c56f6b8b19
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-19 23:00:52 +0000

    Aplicação de configurações genéricas para inicializar a construção do WSDL no sistema

--- 20121/dw/wsl/application/controllers/Error.php
+++ 20121/dw/wsl/application/controllers/Error.php
@@ -1,5 +1,5 @@
 <?php
-class Controller_Error {
+class Controller_Error extends WSL_Controller_ActionAbstract {
     public function errorAction() {}
 }
 
--- 20121/dw/wsl/application/controllers/Index.php
+++ 20121/dw/wsl/application/controllers/Index.php
@@ -2,6 +2,8 @@
 class Controller_Index extends WSL_Controller_ActionAbstract {
     public function indexAction() {
         $discover = new WSL_Soap_AutoDiscover('Service_Users');
+        $this->getResponse()->setHeader('Content-Type', 'application/xml');
+        $this->view->discover = $discover;
     }
 }
 
