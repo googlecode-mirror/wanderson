f92b35bd925ca1247a3e472189b4eff329cd9c6a
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-04-19 23:10:28 +0000

    Correção em cabeçalho de envio HTTP, melhorando o significado do conteúdo.

--- 20121/dw/wsl/application/controllers/Index.php
+++ 20121/dw/wsl/application/controllers/Index.php
@@ -2,7 +2,7 @@
 class Controller_Index extends WSL_Controller_ActionAbstract {
     public function indexAction() {
         $discover = new WSL_Soap_AutoDiscover('Service_Users');
-        $this->getResponse()->setHeader('Content-Type', 'application/xml');
+        $this->getResponse()->setHeader('Content-Type', 'text/xml');
         $this->view->discover = $discover;
     }
 }
