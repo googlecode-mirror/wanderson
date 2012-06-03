603ec559875b1d0e5226adb838c668195ed8f27b
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-06-03 20:14:24 +0000

    Atualização de execução para o gerador de arquivos PDF do compilador, melhorando o tratamento de estado do processo solicitado.

--- trunk/20121/dw/wsl/library/WSL/Compiler/Plugin/Pdf.php
+++ trunk/20121/dw/wsl/library/WSL/Compiler/Plugin/Pdf.php
@@ -29,14 +29,28 @@ class WSL_Compiler_Plugin_Pdf implements WSL_Compiler_PluginAfterInterface {
         $process = proc_open('ps2pdf document.ps', $descriptors, $pipes);
         // Recurso Inicializado?
         if (is_resource($process)) {
-            // Execução de Processo
-            $stdout = stream_get_contents($pipes[1]);
-            $stderr = stream_get_contents($pipes[2]);
+            // Saídas Padrão e de Erro
+            $stdout = '';
+            $stderr = '';
+            // Ocioso
+            $idle = true;
+            // Executar até Finalização
+            while ($idle) {
+                // Tempo Ocioso
+                usleep(500); // 0.5 Segundos
+                // Capturar Saídas
+                $stdout = $stdout . stream_get_contents($pipes[1]);
+                $stderr = $stderr . stream_get_contents($pipes[2]);
+                // Informações sobre Processo
+                $information = proc_get_status($process);
+                // Continuar Ocioso?
+                $idle = (($information !== false) && ($information['running']));
+            }
             // Finalizar Pipes
             fclose($pipes[1]);
             fclose($pipes[2]);
             // Finalizar Processo
-            $result = proc_close($process);
+            proc_close($process);
         }
         // Informar Arquivo de Saída
         $manager->getContext()->setOutput('document.pdf');
