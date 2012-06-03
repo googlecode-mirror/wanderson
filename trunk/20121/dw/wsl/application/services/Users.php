40f19747f04d24fe11ce64000aa4d8136c2b1ca9
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-06-03 23:09:35 +0000

    Modificações em serviço de usuários, removendo a autenticação do sistema.

--- trunk/20121/dw/wsl/application/services/Users.php
+++ trunk/20121/dw/wsl/application/services/Users.php
@@ -9,41 +9,14 @@
 class Service_Users {
 
     /**
-     * Autenticação no Sistema
-     *
-     * Para utilização do Webservice em determinados pontos, precisamos que o
-     * usuário efetue uma autenticação no sistema. Esta autenticação é feita
-     * informando o seu e-mail e hash. Comparados, será disponibilizado um token
-     * para utilização. Este token permanece ativo por tempo determinado.
-     *
-     * @param  string $email E-mail Cadastrado no Sistema
-     * @param  string $hash  Hash Utilizado pelo Usuário
-     * @return string Token Disponibilizado para Autenticação
-     */
-    public function login() {
-        // Filtro de Parâmetros
-        $args = func_get_args();
-        foreach (array('email', 'hash') as $identifier) {
-            $$identifier = array_shift($args);
-        }
-        // Camada de Modelo
-        $model = new Model_Users();
-        // Autenticação
-        $result = $model->login($email, $hash);
-        // Apresentar Resultados
-        return $result;
-    }
-
-    /**
      * Apresentar Usuários Cadastrados
+     *
+     * @param  string $token Token de Autenticação
      * @return array[] Conjunto de Usuários Cadastrados
      */
     public function fetch() {
-        // Filtro de Parâmetros
-        $args = func_get_args();
-        foreach (array('token') as $identifier) {
-            $$identifier = array_shift($args);
-        }
+        // Captura de Parâmetros
+        $token = (string) func_get_arg(0);
         // Camada de Modelo
         $model = new Model_Users();
         // Verificar Credenciais
