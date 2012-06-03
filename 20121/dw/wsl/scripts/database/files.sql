de598e985e662d39c6a65b117a806f258fbe35b4
wandersonwhcr <wandersonwhcr@296847ac-3330-11de-9d05-31133e6853b1>
2012-06-03 20:53:29 +0000

    Alterações em banco de dados para inclusão de registros de documentos gerados pelos usuários, iniciando assim a autenticação concreta no sistema.

--- 20121/dw/wsl/scripts/database/files.sql
+++ 20121/dw/wsl/scripts/database/files.sql
@@ -3,19 +3,19 @@ DROP TABLE IF EXISTS `wsl_files`;
 -- end
 -- begin migration up
 CREATE TABLE IF NOT EXISTS `wsl_files`(
-	-- Chave Primária
-	`id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-	-- Informações de Arquivo
-	`hash` CHAR(40) NOT NULL,
-	`filename` VARCHAR(255) NOT NULL DEFAULT '',
-	-- MetaInformações
-	`container` VARCHAR(20) NOT NULL,
-	`category` VARCHAR(20) NOT NULL,
-	`reference` BIGINT UNSIGNED NOT NULL,
-	`order` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
-	PRIMARY KEY(`id`),
-	INDEX `hash`(`hash`),
-	INDEX `search`(`container`, `category`, `reference`)
+    -- Chave Primária
+    `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
+    -- Informações de Arquivo
+    `hash` CHAR(40) NOT NULL,
+    `filename` VARCHAR(255) NOT NULL DEFAULT '',
+    -- MetaInformações
+    `container` VARCHAR(20) NOT NULL,
+    `category` VARCHAR(20) NOT NULL,
+    `reference` BIGINT UNSIGNED NOT NULL,
+    `order` SMALLINT UNSIGNED NOT NULL DEFAULT 0,
+    PRIMARY KEY(`id`),
+    INDEX `hash`(`hash`),
+    INDEX `search`(`container`, `category`, `reference`)
 )
 ENGINE = MyISAM
 DEFAULT CHARACTER SET = utf8
--- 20121/dw/wsl/scripts/database/users.sql
+++ 20121/dw/wsl/scripts/database/users.sql
@@ -1,17 +1,18 @@
 -- begin migration down
 DROP TABLE IF EXISTS `wsl_users`;
 -- end
-
 -- begin migration up
 CREATE TABLE IF NOT EXISTS `wsl_users`(
+    -- Chave Primária
     `id` BIGINT UNSIGNED AUTO_INCREMENT NOT NULL,
+    -- Informações
     `hash` CHAR(40) NOT NULL UNIQUE,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `active` TINYINT UNSIGNED NOT NULL DEFAULT 1,
     `admin` TINYINT UNSIGNED NOT NULL DEFAULT 0,
-    `session` TIMESTAMP NULL,
-    `token` CHAR(40) NULL UNIQUE,
-    PRIMARY KEY(`id`)
+    -- Chaves
+    PRIMARY KEY(`id`),
+    INDEX `credentials`(`hash`,`email`)
 )
 ENGINE = InnoDB
 DEFAULT CHARACTER SET = utf8
